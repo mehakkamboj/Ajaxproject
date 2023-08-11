17bf85bff94ce4bdc57657887d04d023dc3a3f57
mehakkamboj <mehakkamboj53@gmail.com>
2023-08-11 23:02:19 +0530

    final project

---
+++ main.js
@@ -0,0 +1,63 @@
+let term = '';
+const songContainer = document.getElementById('songs');
+
+const updateTerm = () => {
+    term = document.getElementById('searchInput').value;
+    if (!term || term === '') {
+        alert ('Please enter a search term');
+    } else {
+
+        while(songContainer.firstChild) {
+            songContainer.removeChild(songContainer.firstChild);
+        }
+
+        const url = `https://itunes.apple.com/search?country=US&term=${term}&media=music&limit=50`
+        fetch(url)
+        .then((response)=> response.json() )
+        .then((data) => {
+            // console.log(data);
+            const artists = data.results;
+            return artists.map(result => {
+                const article = document.createElement('article'),
+                artist = document.createElement('p'),
+                song = document.createElement('p'),
+                img = document.createElement('img'),
+                audio = document.createElement('audio'),
+                audioSource = document.createElement('source')
+
+                artist.innerHTML = result.artistName;
+                song.innerHTML = result.trackName;
+                img.src = result.artworkUrl100;
+                audioSource.src = result.previewUrl;
+                audio.setAttribute('controls','');
+
+                article.appendChild(img)
+                article.appendChild(artist)
+                article.appendChild(song)
+                article.appendChild(audio)
+                audio.appendChild(audioSource)
+                songContainer.appendChild(article);
+
+         
+
+         
+    })
+})
+    }
+}
+
+const searchBtn = document.querySelector('button');
+searchBtn.addEventListener('click', updateTerm);
+
+document.addEventListener('play', event => {
+    const audio = document.getElementsByTagName('audio');
+    for(let i = 0; i < audio.length; i++) {
+        if(audio[i] != event.target) {
+            audio[i].pause();
+        }
+    }
+}, true)
+
+
+
+// .catch(error => console.log('Request Failed: ', error))
